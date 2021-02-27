bad6e820a6b4cf71fc8b0e49f27d39c42e0a227b
belmiro-kunga <belmirokunga@gmail.com>
2021-02-27 17:41:13 +0100

    Variáveis JavaScript
    Muito parecido com álgebra,  Identificadores JavaScript, O Operador de Atribuição, Tipos de dados JavaScript, Declarando (Criando) Variáveis JavaScript, Uma Declaração, Muitas Variáveis

---
+++ Variaveis_JavaScript/index.js
@@ -0,0 +1,49 @@
+/*
+var x = 4;
+var y = 7;
+var z = x + y;
+
+document.getElementById("demo").innerHTML =" o valor de z é " + z ;
+*/
+/*
+var preco01 = 4;
+var preco02 = 6;
+var precoTotal = preco01 + preco02;
+document.getElementById("demo").innerHTML = "O toatal é " + precoTotal 
+/*
+/*
+var smartphone = "Samsung";
+
+document.getElementById("demo").innerHTML = smartphone;
+*/
+
+/*
+var proprietario = "Austin", carName = "volvo", preco = 50.000 + "kz";
+
+document.getElementById("demo").innerHTML = carName;
+*/
+/*
+var carName;
+
+document.getElementById("demo").innerHTML = carName;
+*/ 
+/*
+var carName = "toyota";
+var carName;
+
+document.getElementById("demo").innerHTML = carName;
+*/ 
+/*
+var x = 5 + 3 + 4;
+
+document.getElementById("demo").innerHTML = x;
+*/
+/*
+var x = "joão " + "" + " Alberto";
+
+document.getElementById("demo").innerHTML = x;
+
+*/
+
+x = "5" + 2 + 3;
+document.getElementById("demo").innerText = x;
