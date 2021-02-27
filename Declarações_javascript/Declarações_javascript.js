d17bbd4f7f0ac428a22ed0cc4c4938c3e8bd9740
belmiro-kunga <belmirokunga@gmail.com>
2021-02-27 15:45:07 +0100

    Declarações javascript
    Ponto e vírgula; Espaço Branco JavaScript Comprimento da linha JavaScript e quebras de linha, Blocos de código JavaScript

---
+++ Declarações_javascript/Declarações_javascript.js
@@ -0,0 +1,23 @@
+/*var x, y, z;
+x = 4;
+y = 4;
+z = x + y;
+
+
+document.getElementById("demo").innerHTML =
+"O valo de Z é  " + z + ".";  
+
+*/
+
+/*
+var a, b, c;
+a = 9; b = 5; c = a + b;
+
+document.getElementById("demo1").innerHTML = c;
+
+*/
+
+
+var pessoa = Aluno;
+var aula = Programcao;
+
