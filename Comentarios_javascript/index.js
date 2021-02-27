e5dcc6b80765571a9db4aeacf013f3507afb016b
belmiro-kunga <belmirokunga@gmail.com>
2021-02-27 16:40:39 +0100

    Comentários javascript
    Comentários de linha única,  Comentários multi-linha

---
+++ Comentarios_javascript/index.js
@@ -0,0 +1,12 @@
+// Alterar o titulo
+document.getElementById("coment").innerHTML = "Comentario Js";
+
+// Mudar o paragrafo
+document.getElementById("mycoment").innerHTML = "Meu primeiro paragrafo"
+
+/*
+document.getElementById("coment").innerHTML = "Comentario Js";
+document.getElementById("mycoment").innerHTML = "Meu primeiro paragrafo"
+*/
+
+
