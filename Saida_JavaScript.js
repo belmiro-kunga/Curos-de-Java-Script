a6774106772b3f18eb9202447f3f37dab523c510
belmiro-kunga <belmirokunga@gmail.com>
2021-02-27 14:47:34 +0100

    Saída JavaScript
    Saída JavaScript com : Usando o innerHTML, Usando document.write(),  Usando window.alert(), Usando console.log()

---
+++ Saida_JavaScript.js
@@ -0,0 +1,19 @@
+/*
+Escrevendo em um elemento HTML, usando .innerHTML
+document.getElementById("demo").innerHTML = 5 + 5;*/
+/*
+  Escrevendo na saída HTML usando .document.write
+
+  document.write("5 + 5");
+*/
+/*
+Escrevendo em uma caixa de alerta, usando .window.alert()
+
+alert("Ola a todos os alunos!")
+*/
+
+
+console.log( 4 * 5);
+
+
+
