716f55e5d9b1aaf0e6f1243e83cf3ce5f9a0ab17
belmiro-kunga <belmirokunga@gmail.com>
2021-02-27 16:27:12 +0100

    Sintaxe JavaScript
    Valores javascript,  Variáveis JavaScript,
    Operadores JavaScript

---
+++ Sintaxe_JavaScript/index.js
@@ -0,0 +1,20 @@
+/*document.getElementById("demo").innerHTML = 10.50;
+*/
+
+/*document.getElementById("demo").innerHTML = "Manuel Andre";
+*/
+/*
+var x;
+x = 5;
+document.getElementById("demo").innerHTML = x * 20;
+*/
+/*
+document.getElementById("demo").innerHTML = "Manuel" + "" + " Kunga";
+*/
+
+
+var x, y;
+x = 5 + 4;
+y = x + 10;
+
+document.getElementById("demo").innerHTML = y;
