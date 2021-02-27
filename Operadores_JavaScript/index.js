4f4a79d71892ad4bf09c985cedc3948822df9126
belmiro-kunga <belmirokunga@gmail.com>
2021-02-27 18:29:25 +0100

    Operadores JavaScript
    Operadores aritméticos JavaScript,  Operadores de atribuição JavaScript

---
+++ Operadores_JavaScript/index.js
@@ -0,0 +1,46 @@
+/*var x = 2;
+var y = 3;
+var z = x + y;
+document.getElementById("demo").innerHTML = z;
+*/
+
+/*
+var x = 12;
+var y = 3;
+var z = x - y;
+document.getElementById("demo").innerHTML = z;
+
+*/
+/*
+var x = 12;
+var y = 3;
+var z = x * y;
+document.getElementById("demo").innerHTML = z;
+*/
+
+/*
+var x = 12;
+var y = 3;
+var z = x / y;
+document.getElementById("demo").innerHTML = z;
+*/
+/*
+var x = 10;
+x += 5;
+document.getElementById("demo").innerHTML = x;
+*/
+/*
+Desafio
+var txt1 = gilbero;
+var txt2 = ana;
+
+var txt3 = txt1 + "" + txt2;
+*/
+
+/*
+Desafio
+var x = 5 + 5 ;
+var y = "3" + 4;
+var z = hello word + 5;
+
+*/
