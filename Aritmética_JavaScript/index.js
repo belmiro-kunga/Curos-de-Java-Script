94b204085396e8fb710ca7f4a5861ec793eb1ce1
belmiro-kunga <belmirokunga@gmail.com>
2021-02-28 14:11:01 +0100

    Aritmética JavaScript
    Operadores aritméticos JavaScript, Operações Aritméticas

---
+++ Aritmética_JavaScript/index.js
@@ -0,0 +1,41 @@
+/*
+var x = 100 / 50;
+document.getElementById("demo").innerHTML = x;
+
+
+var a = 4;
+var x = (100 + 5) * a;
+document.getElementById("demo1").innerHTML = x;
+
+
+var a = 4;
+var x = (100 - 5) / a;
+document.getElementById("demo2").innerHTML = x;
+
+var x = 4;
+var y = 8;
+var z = x % y;
+document.getElementById("demo3").innerHTML = z;
+
+var x = 5;
+x++;
+var z = x;
+document.getElementById("demo4").innerHTML = z;
+
+var x = 5;
+x--;
+var z = x;
+document.getElementById("demo4").innerHTML = z;
+
+
+var x = 5;
+document.getElementById("demo").innerHTML = x ** 2;
+
+
+
+var x = 5;
+document.getElementById("demo").innerHTML = Math.pow(x,2);
+ 
+*/
+
+ 
