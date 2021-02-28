16c595d9ac128829895781d79f2b680810c993c7
belmiro-kunga <belmirokunga@gmail.com>
2021-02-28 14:31:57 +0100

    Atribuição JavaScript
    Operadores de atribuição JavaScript, Exemplos de atribuição

---
+++ Atribuicao_JavaScript/index.js
@@ -0,0 +1,20 @@
+/*
+var x = 11;
+x += 4;
+document.getElementById("demo").innerHTML = x;
+
+
+var x = 11;
+x -= 4;
+document.getElementById("demo").innerHTML = x;
+
+
+var x = 11;
+x *= 4;
+document.getElementById("demo").innerHTML = x;
+
+*/
+
+var x = 11;
+x /= 4;
+document.getElementById("demo").innerHTML = x;
