20ce9d19a66637daf6fa2b236200d953998cc504
belmiro-kunga <belmirokunga@gmail.com>
2021-03-02 01:41:25 +0100

    javascript

---
+++ Tipos_de_dados_JavaScript/index.js
@@ -0,0 +1,66 @@
+/*var x = 14 + " carro";
+document.getElementById("demo").innerHTML = x;
+
+var x = 12 + 3 + " casa";
+document.getElementById("demo").innerHTML = x;
+
+
+var x = " casa" + 12 + 3;
+document.getElementById("demo").innerHTML = x;
+
+
+var x;
+x = 5;
+x  = "kunga";
+document.getElementById("demo").innerHTML = x;
+
+
+var casaGrande = "minha casa";
+var carro = '2';
+
+
+var x1 = 23.00;
+var x2 = 34;
+var x3 = 2.8;
+document.getElementById("demo").innerHTML = x1 + "<br>" + x2 + "<br>" + x3;
+
+var x = 5;
+var y = 6;
+var z = 7;
+document.getElementById("demo").innerHTML = (x == y) + "<br>" + (x == z);[]
+
+var carro = ["carro de lata", "BMW", "subaru"]
+document.getElementById("demo").innerHTML =
+carro[0]
+
+
+
+var pessoa = { 
+primeiroNome : "Belmiro ",
+segundoNome : "kunga",
+idade : 18,
+corDapel : "castano"
+};
+document.getElementById("demo").innerHTML =
+pessoa.primeiroNome + " tem " +  pessoa.idade + " de idade ";
+
+---------------------------------------------
+
+var carro = "";
+document.getElementById("demo").innerHTML =
+"este valor é " + carro + "<br>" + " o tipo da variavel é " + typeof carro;
+-----------------------------------------
+*/
+
+var pessoa = {
+    primeiroNome: "belio",
+    segundoNome :" bulula",
+    idade : 18,
+    corOlhos : "castanhos"
+};
+    document.getElementById("demo").innerHTML = typeof pessoa
+
+
+
+
+
