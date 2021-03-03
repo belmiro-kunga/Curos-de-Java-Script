6a21ccea973e829020bc1672cf37912d4a815c6f
belmiro-kunga <belmirokunga@gmail.com>
2021-03-03 02:42:42 +0100

    Variaveis
    var, let, e cosnt

--- Variaveis_JavaScript/index.js
+++ Variaveis_JavaScript/index.js
@@ -1,49 +1,145 @@
-/*
-var x = 4;
-var y = 7;
+/* Neste exemplo, x, yeoz são variáveis
+var x = 5;
+var y = 12;
 var z = x + y;
 
-document.getElementById("demo").innerHTML =" o valor de z é " + z ;
+document.getElementById("demo").innerHTML = " O valor de z é: "  + z;
 */
 /*
-var preco01 = 4;
-var preco02 = 6;
-var precoTotal = preco01 + preco02;
-document.getElementById("demo").innerHTML = "O toatal é " + precoTotal 
-/*
-/*
-var smartphone = "Samsung";
-
-document.getElementById("demo").innerHTML = smartphone;
+Usando let e const (ES6)
+let x = 23;
+let y = 3;
+let z = x + y;
+document.getElementById("demo").innerHTML = " O valor de z é: "  + z;
 */
 
 /*
-var proprietario = "Austin", carName = "volvo", preco = 50.000 + "kz";
-
-document.getElementById("demo").innerHTML = carName;
+const x = 32;
+const y = 24;
+const z = x + y;
+document.getElementById("demo").innerHTML = " O valor de z é: "  + z;
 */
 /*
-var carName;
 
-document.getElementById("demo").innerHTML = carName;
-*/ 
-/*
-var carName = "toyota";
-var carName;
-
-document.getElementById("demo").innerHTML = carName;
-*/ 
-/*
-var x = 5 + 3 + 4;
-
-document.getElementById("demo").innerHTML = x;
+var preco1 = 100;
+var preco2 = 200;
+var total = preco1 + preco2;
+document.getElementById("demo").innerHTML = " O valor de total é: "  + total;
 */
 /*
-var x = "joão " + "" + " Alberto";
+Erro ao declarar uma variavel
+var 1numero =  12;
+var = casa grande = 13;
+var -casa = "grande";
+*/
 
-document.getElementById("demo").innerHTML = x;
+/*
+forma corretra de declarar uma variavel
+var caroNovoAmarelo = "toyta" ;
+var caronovoamarelo = 'cala';
+var caro_novo = 12;
 
 */
 
-x = "5" + 2 + 3;
-document.getElementById("demo").innerText = x;
+
+///////////// JavaScript Let /////////////////////
+
+/*
+Escopo global
+var nomeDeCarro = "carro de lata";
+minhaFuncao ()
+
+function minhaFuncao(){
+ document.getElementById("demo").innerHTML = "Este carro é mesmo de lata" + nomeDeCarro;
+}
+*/
+
+/*
+Escopo da Função
+meuCarro()
+function meuCarro(){
+    var nomeDoCarro = " carro de lata ";
+    document.getElementById("demo1").innerHTML = typeof  nomeDoCarro + " " + nomeDoCarro
+}
+
+document.getElementById("demo2").innerHTML = typeof nomeDoCarro;
+*/
+/*
+var x = 10;
+{
+    var x = 2;
+}
+document.getElementById("demo1").innerHTML = x;
+*/
+
+/*
+Variaveis de escopo
+var x = 12;
+ 
+{
+    let x = 2;
+}
+document.getElementById("demo1").innerHTML = x;
+*/
+
+
+///////////// JavaScript const /////////////////////
+/*
+try {
+    const PI = 3.1415;
+    
+}
+catch (err){
+    document.getElementById("demo1").innerHTML = err;
+
+}
+*/
+
+/*
+var x = 10;
+
+{
+   const x = 2;
+}
+
+document.getElementById("demo1").innerHTML = x;
+*/
+/*
+const x;
+x = 3;
+document.getElementById("demo1").innerHTML = x;
+*/
+/*
+ const carro = {marca: "fiat", model:12, cor: "banco"};
+
+ carro.cor = "vermelho";
+ carro.proprietario = "Belmiro";
+
+ document.getElementById("demo1").innerHTML = "o dono do carro é o " + carro.proprietario;
+
+ */
+/*
+try {
+    const carro = {marca: "fiat", model:12, cor: "banco"};
+    carro = {marca: "Toyota", model:12, cor: "banco"};
+}
+catch(err){
+    document.getElementById("demo1").innerHTML = err
+}
+
+ */
+/*
+const carro = ["fiat ", "Volvo ", "BMW"];
+
+ carro[0] = "Toyota";
+ 
+ carro.push (" Audi");
+ document.getElementById("demo1").innerHTML = carro;
+ */
+try {
+    const carro = ["Saab", "Volvo", "BMW"];
+    carro = ["Toyota", "Volvo", "Audi"];
+  }
+  catch (err) {
+    document.getElementById("demo1").innerHTML = err;
+  }
